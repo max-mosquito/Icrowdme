26f508224e7adf142129710eeaed89f7ccf587f1
max-mosquito <67696697+max-mosquito@users.noreply.github.com>
2020-07-06 16:55:24 +0300

    icrowdme

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.slider').slick();
+	$(".tab_item").not(":first").hide();
+	$(".wrapper .tab").click(function() {
+		$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active")
+});
