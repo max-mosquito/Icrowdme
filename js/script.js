81862ed418ae7fe9010ecc6d4a41fd2703c2e35e
max-mosquito <67696697+max-mosquito@users.noreply.github.com>
2020-07-07 14:34:42 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 	$('.slider').slick();
+
 	$(".tab_item").not(":first").hide();
 	$(".wrapper .tab").click(function() {
 		$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tab_item").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active")
+	}).eq(0).addClass("active");
+
+	$('.burger').click(function(event){
+		$('.burger, .nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
